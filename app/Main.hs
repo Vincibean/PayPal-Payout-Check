bc1c89f891c83ae406a3d3c5afb057351ad80cd1
Vincibean <andrebessi00@gmail.com>
2020-02-16 23:48:45 +0000

    made the script specific to paypal login

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,31 @@
 
 module Main where
 
-import Lib
+import Control.Concurrent
+import Control.Monad.State
 import Test.WebDriver
+import Test.WebDriver.Session
 
 main :: IO ()
 main = runSession chromeConfig $ do
-  openPage "http://google.com"
-  searchInput <- findElem ( ByCSS "input[type='text']" )
-  sendKeys "Hello, World!" searchInput
-  submit searchInput
+  openPage "https://www.paypal.com/login"
+  usr <- findElem ( ById "email" )
+  sendKeys "usr" usr
+  btnNext <- findElem ( ById "btnNext" )
+  click btnNext
+  wait 1000000
+  pwd <- findElem ( ById "password" )
+  sendKeys "pwd" pwd
+  btnLogin <- findElem ( ById "btnLogin" )
+  click btnLogin
   closeSession
 
 
 chromeConfig :: WDConfig
 chromeConfig = useBrowser chrome defaultConfig
+
+wait :: Int -> WD ()
+wait x = io $ threadDelay x
+
+io :: IO a -> WD a
+io = WD . liftIO
