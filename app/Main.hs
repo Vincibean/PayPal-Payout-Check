6021bb7032af143f4e7fb1b62dc0e029b832bd31
Vincibean <andrebessi00@gmail.com>
2020-02-17 19:38:19 +0000

    cosmetic changes

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,9 @@ import Test.WebDriver.Session
 main :: IO ()
 main = runSession chromeConfig $ do
   login
-  wait 5000000
+  wait $ 5 * seconds
   logout
-  wait 2000000
+  wait $ 2 * seconds
   closeSession
 
 login :: WD ()
@@ -20,14 +20,14 @@ login = do
   openPage "https://www.paypal.com/login"
   usr <- findElem ( ById "email" )
   sendKeys "usr" usr
-  wait 1000000
+  wait $ 1 * seconds
   btnNext <- findElem ( ById "btnNext" )
   click btnNext
-  wait 2500000
+  wait $ 3 * seconds
   pwd <- findElem ( ById "password" )
   sendKeys "pwd" pwd
   btnLogin <- findElem ( ById "btnLogin" )
-  wait 1000000
+  wait $ 1 * second
   click btnLogin
 
 logout :: WD ()
@@ -35,6 +35,11 @@ logout = do
   btnLogout <- findElem ( ByClass "css-1fo6ps" )
   click btnLogout
 
+second :: Int
+second = 1000000
+
+seconds :: Int
+seconds = second
 
 chromeConfig :: WDConfig
 chromeConfig = useBrowser chrome defaultConfig
