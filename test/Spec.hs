3400ebfdf87533bcd6c397e31cf743cc300a27b5
Vincibean <andrebessi00@gmail.com>
2020-02-21 00:01:20 +0000

    tinkering with lens-aeson

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Hspec
+import Control.Lens
+import Data.Aeson.Lens
+import qualified Data.ByteString as B
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Aeson" $ do
+      it "tinkerin with Aeson" $ do
+        bs <- B.readFile "test-resources/colors.json"
+        let exp = ["red","black"]
+        let res = bs ^.. values . key "color" . _String
+        res `shouldBe` exp
