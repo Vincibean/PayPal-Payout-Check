1f1b057859614e2b28aef69e735f737b50d93daf
Vincibean <andrebessi00@gmail.com>
2020-03-01 23:35:32 +0000

    more tinkering with aeson-lens

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,8 +8,13 @@ import qualified Data.ByteString as B
 main :: IO ()
 main = hspec $ do
   describe "Aeson" $ do
-      it "tinkerin with Aeson" $ do
+      it "tinkerin with Aeson (colors.json)" $ do
         bs <- B.readFile "test-resources/colors.json"
         let exp = ["red","black"]
-        let res = bs ^.. values . key "color" . _String
+        let res = bs^..values.key "color"._String
+        res `shouldBe` exp
+      it "tinkerin with Aeson (data.json)" $ do
+        bs <- B.readFile "test-resources/data.json"
+        let exp = ["MPA-8XFV5Q9HR5STQ","MPA-FG3CJBY74AY84","MPA-2BC62X5BCSPL6","MPA-NCMYMPFNJG4RU","MPA-ELPBVE29KFNRL","MPA-9T33RW8E75756","MPA-76SNKUN48ZBTA","MPA-NS9ZT8SBN8MRE","MPA-EJMHER2PWZELW","MPA-ZKJR92DLGR3NU","MPA-UFY3JZ9CF4UUJ","MPA-UK6ZZWHMGNN6L"]
+        let res = bs ^.. key "data".key "transactions".values.key "transactionId"._String
         res `shouldBe` exp
